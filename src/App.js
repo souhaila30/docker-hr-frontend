93dda56924c1c431e04fa523c32f2e61fc16d7a3
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-03 23:09:47 -0400

    working code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-//import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import './App.css';
+
 import EmailField from './emailfield.js';
 import Title from './hello.js';
 import FetchData from './FetchData.js';
-import Header from './Header.js';
+//import Header from './Header.js';
+//import TableOutput from './CreateTable.js';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-        title:"Heart Rate Monitor",
-        };
-    };
-
   render() {
     return (
       <div>
-        <Header title={this.state.title} />
         <Title />
         <EmailField />
         <FetchData />
