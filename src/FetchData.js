bcb4bf09763dc82af02f603783518f92921b17e2
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-03 23:10:00 -0400

    broken code

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import axios from 'axios';
 import Button from 'material-ui/Button';
+import ReactTable from 'react-table'
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui/Table';
+
+var styles = {
+  "dataStyle": {
+        "marginTop":"20px",
+        "marginbottom":"20px",
+        "color":"blue",
+  }
+}
 
 class FetchData extends React.Component {
     constructor() {
         super();
-        this.state = {
-          "data": ["Nothing yet"],
+        this.state = {email : "none",
+          url: "http://vcm-3586.vm.duke.edu:5000/api/heart_rate/",
+          data: ['nothing yet']
         };
-    }
+    
+
+    changeEmail(email) {this.setState({email})};
+    completeUrl = this.state.url + this.state.email
 
     getRequest = () => {
-            axios.get("http://vcm-3586.vm.duke.edu:5000/api/heart_rate/").then( (response) => {
+            axios.get(completeUrl).then( (response) => {
                     console.log(response);
                     console.log(response.status);
                     this.setState({"data": response.data});
             })
     }
-          
+        
     render() {
       return (
             <div>
@@ -25,12 +46,13 @@ class FetchData extends React.Component {
                     Get Heart Rate Data
                 </Button>
 
-                <div>
+                <div style={styles.dataStyle}>
                     {this.state.data}
                 </div>
-            </div>
-      )
+        </div>
+      )                
     }
-}
+};
 
 export default FetchData;
+
