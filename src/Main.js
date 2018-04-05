d664fb3550d567a8870e7ebddbeb7a9fa4c84065
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-04 20:17:18 -0400

    working code for getting user and hr, table code brokem

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
+import Table, {TableBody, TableCell, TableHead, TableRow} from 'material-ui/Table';
 
 export default class Main extends React.Component {
   constructor() {
@@ -9,6 +10,7 @@ export default class Main extends React.Component {
     this.state = {
       "emailTextField":"empty",
       "data":["nothing yet!!"],
+      "url":"",
     };
   }
 
@@ -22,16 +24,34 @@ export default class Main extends React.Component {
   }
 
 
-  url ="http://vcm-3586.vm.duke.edu:5000/api/heart_rate/"
+//  var newUrl ="http://vcm-3586.vm.duke.edu:5000/api/heart_rate/" + this.state.emailTextField
 
   getData = () => {
-    axios.get(${url}${this.state.emailTextField}).then( (response) => {
-      console.log(respose);
+    var newUrl ="http://vcm-3586.vm.duke.edu:5000/api/heart_rate/" + this.state.emailTextField
+    axios.get(newUrl).then( (response) => {
+      console.log(response);
       console.log(response.status);
       this.setState({"data":response.data});
     })
+    return this.state.data
+  }
+  
+  generateTable = () => {
+    var TableData = [];
+    for (var i = 0; i < this.state.data.length; i++){
+      TableData.push(
+        <TableRow>
+            <TableCell> this.state.data[0] </TableCell>
+            <TableCell> this.state.data[1] </TableCell>
+        </TableRow>
+      )
+    };
+
+    return TableData;
   }
 
+
+  
     render() {
       return (
         <div>
@@ -46,6 +66,17 @@ export default class Main extends React.Component {
             <div>
                 {this.state.data}
             </div>
+            
+            var TableData = this.generateTable();
+            <Table>
+                    <TableHead>
+                                <TableRow>
+                                            <TableCell> Heart Rate(bpm) </TableCell>
+                                            <TableCell> Time </TableCell>
+                                </TableRow>
+                    </TableHead>
+                    {TableData}
+            </Table>
         </div>
       );
     }
