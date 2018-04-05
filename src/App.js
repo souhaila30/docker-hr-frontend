23430f2def33254b0a597265707ef72032b3d916
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-04 20:18:21 -0400

    working

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import './App.css';
 
-import EmailField from './emailfield.js';
+//import EmailField from './emailfield.js';
 import Title from './hello.js';
-import FetchData from './FetchData.js';
+//import FetchData from './FetchData.js';
 //import Header from './Header.js';
 //import TableOutput from './CreateTable.js';
+import Main from './Main.js';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Title />
-        <EmailField />
-        <FetchData />
+        <Main />
       </div>
     );
   }
