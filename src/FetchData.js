4c49ec31c6b7743249aaa3b440f81393154884dc
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-04 20:18:11 -0400

    code broken

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+//import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+/*
 import ReactTable from 'react-table'
 import {
     Table,
@@ -10,49 +13,65 @@ import {
     TableRow,
     TableRowColumn,
 } from 'material-ui/Table';
+*/
+//import EmailField from './emailfield.js'
 
 var styles = {
   "dataStyle": {
-        "marginTop":"20px",
+        "marginTop":"120px",
         "marginbottom":"20px",
         "color":"blue",
   }
 }
 
+var url = ""
+
 class FetchData extends React.Component {
     constructor() {
         super();
-        this.state = {email : "none",
-          url: "http://vcm-3586.vm.duke.edu:5000/api/heart_rate/",
-          data: ['nothing yet']
+        this.state = {
+          "emailTextField": "sa@duke.edu",
+          "data": ['nothing yet'],
         };
+    }
     
+    url= "http://vcm-3586.vm.duke.edu:5000/api/heart_rate/"
+    
+    onEmailTextFieldChange = (event) => {
+      this.setState({"emailTextField":event.target.value});
+    }
 
-    changeEmail(email) {this.setState({email})};
-    completeUrl = this.state.url + this.state.email
+    onButtonClick = (event) => {
+      console.log(this.state.emailTextField);
+    }
 
-    getRequest = () => {
-            axios.get(completeUrl).then( (response) => {
+        url  =`url ${this.state.emailTextField}`
+        getData = () => {
+            axios.get(url).then( (response) => {
                     console.log(response);
                     console.log(response.status);
                     this.setState({"data": response.data});
             })
-    }
-        
+        }
+
     render() {
       return (
             <div>
-                <Button variant='raised' onClick={this.getRequest}>
+                <TextField
+                    value={this.state.emailTextField}
+                    onChange={this.onEmailTextFieldChange}/>
+
+                <Button variant="raised" onClick={this.onButtonClick}>
                     Get Heart Rate Data
                 </Button>
 
                 <div style={styles.dataStyle}>
+                    {url}
                     {this.state.data}
                 </div>
-        </div>
+            </div>
       )                
     }
-};
+}
 
 export default FetchData;
-
