f3bcfbfca3322ee04fc425e3c4e75166f49d1d98
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-06 14:48:44 -0400

    line graph working

--- src/Main.js
+++ src/Main.js
@@ -6,19 +6,14 @@ import Table, {TableCell, TableHead, TableRow} from 'material-ui/Table';
 import Typography from 'material-ui/Typography';
 import Toolbar from 'material-ui/Toolbar';
 import AppBar from 'material-ui/AppBar';
-import lightBlue from 'material-ui/colors/lightBlue';
-import blue from 'material-ui/colors/blue';
 import {Line} from 'react-chartjs-2';
 
 var styles = {
   "appBarStyle": {
     "marginBottom": "30px",
     "marginLeft":"10px",
-    "backgroundColor": "lightBlue",
-  }
-}
-
-var styles = {
+    "backgroundColor": "blue",
+  },
    "textFieldStyle": {
         "marginTop": "50px",
         "marginLeft":"300px",
@@ -76,35 +71,40 @@ export default class Main extends React.Component {
     return tabledata;
   };
 
-  var  graph_data = {
-    labels: ["Heart Rate", "Time"],
-    datasets: [
-      {
-        label: "User's Heart Rate Measurements",
-        fill: false,
-        lineTension: 0.1,
-        backgroubackgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: this.state.data[0]
-      }
-    ]
+  createGraph = () => {
+
+    var graph_data = {
+        labels: this.state.data[1],
+        datasets: [
+        {
+            label: " Heart Rate (bpm)",
+            fill: false,
+            lineTension: 0.1,
+            backgroubackgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: this.state.data[0]
+            }
+        ]
+    }
+    return graph_data;
   };
 
     render() {
         var tabledata = this.generateTable();
+        var graph_data = this.createGraph();
       return (
         <div>
         <AppBar position="static" style={styles.appBarStyle}>
