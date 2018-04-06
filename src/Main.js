c180281ee982e7d5e82413ea4c55a9588d8779e1
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-06 14:05:01 -0400

    working code

--- src/Main.js
+++ src/Main.js
@@ -2,15 +2,38 @@ import React from 'react';
 import axios from 'axios';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
-import Table, {TableBody, TableCell, TableHead, TableRow} from 'material-ui/Table';
+import Table, {TableCell, TableHead, TableRow} from 'material-ui/Table';
+import Typography from 'material-ui/Typography';
+import Toolbar from 'material-ui/Toolbar';
+import AppBar from 'material-ui/AppBar';
+import lightBlue from 'material-ui/colors/lightBlue';
+import blue from 'material-ui/colors/blue';
+
+var styles = {
+  "appBarStyle": {
+    "marginBottom": "30px",
+    "marginLeft":"10px",
+    "backgroundColor": "lightBlue",
+  }
+}
+
+var styles = {
+   "textFieldStyle": {
+        "marginTop": "50px",
+        "marginLeft":"300px",
+        "backgroundColor": "lightBlue",
+    }
+  }
 
 export default class Main extends React.Component {
   constructor() {
     super();
     this.state = {
-      "emailTextField":"empty",
-      "data":["nothing yet!!"],
+      "emailTextField":"",
+      "data":[],
       "url":"",
+      "hr":[],
+      "hr_times":[],
     };
   }
 
@@ -24,50 +47,55 @@ export default class Main extends React.Component {
   }
 
 
-//  var newUrl ="http://vcm-3586.vm.duke.edu:5000/api/heart_rate/" + this.state.emailTextField
-
   getData = () => {
     var newUrl ="http://vcm-3586.vm.duke.edu:5000/api/heart_rate/" + this.state.emailTextField
     axios.get(newUrl).then( (response) => {
       console.log(response);
       console.log(response.status);
-      this.setState({"data":response.data});
+    this.setState({"data":response.data});
     })
-    return this.state.data
-  }
-  
+    return this.state.hr
+  }    
+
   generateTable = () => {
-    var TableData = [];
-    for (var i = 0; i < this.state.data.length; i++){
-      TableData.push(
+    if (this.state.data.length === 0) {
+        return [];
+    }
+    var tabledata = [];
+    console.log(this.state.data)
+    for (var i = 0; i < this.state.data[0].length; i++){
+      tabledata.push(
         <TableRow>
-            <TableCell> this.state.data[0] </TableCell>
-            <TableCell> this.state.data[1] </TableCell>
+            <TableCell> {this.state.data[0][i]} </TableCell>
+            <TableCell> {this.state.data[1][i]} </TableCell>
         </TableRow>
-      )
-    };
+      );
+    }
 
-    return TableData;
+    return tabledata;
   }
-
 
   
     render() {
+        var tabledata = this.generateTable();
       return (
         <div>
-            <TextField
+        <AppBar position="static" style={styles.appBarStyle}>
+            <Toolbar>
+                <Typography variant="display3" gutterBottom align="right">
+                    Heart Data User App
+                </Typography>
+            </Toolbar>
+        </AppBar>
+            
+            <TextField style={styles.textFieldStyle}
                 value={this.state.emailTextField}
                 onChange={this.onEmailTextFieldChange}/>
-
-            <Button variant="raised" onClick={this.getData}>
-                Get Heart Rates 
-            </Button>
-
-            <div>
-                {this.state.data}
-            </div>
             
-            var TableData = this.generateTable();
+            <Button variant="raised" onClick={this.getData}>
+                Get User Heart Rates 
+            </Button>
+            
             <Table>
                     <TableHead>
                                 <TableRow>
@@ -75,7 +103,7 @@ export default class Main extends React.Component {
                                             <TableCell> Time </TableCell>
                                 </TableRow>
                     </TableHead>
-                    {TableData}
+                    {tabledata}
             </Table>
         </div>
       );
